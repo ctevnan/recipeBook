a35e8e984b8e90711e9065ea19c5963d3b759fc8
ctevnan <carolyntevnan@yahoo.com>
2016-10-25 12:47:12 -0400

    adding correct file structure to app.module.ts, adding recipe-detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header.component';
 import { RecipesComponent } from './recipes';
-import { RecipeItemComponent } from './recipe-item.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item.component';
+import { RecipeListComponent } from './recipes/recipe-list';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     RecipesComponent,
-    RecipeItemComponent
+    RecipeItemComponent,
+    RecipeListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: 'module.id',
+  selector: 'rb-recipe-detail',
+  templateUrl: 'recipe-detail.component.html',
+})
+export class RecipeDetailComponent implements OnInit {
+  
+  constructor() {}
+
+  ngOnInit() {
+  }
+}
