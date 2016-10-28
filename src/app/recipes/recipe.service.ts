58eb1c2e1974fe8eaf103e5a4c680f6f9eae893f
ctevnan <carolyntevnan@yahoo.com>
2016-10-28 12:14:00 -0400

    adding ingredients

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@angular/core';
 
 import { Recipe } from "./recipe";
+import { Ingredient } from "../shared";
 
 @Injectable()
 export class RecipeService {
   private recipes: Recipe[] = [
-    new Recipe('Schnitzel', 'Very tasty', 'http://www.giftforyou.nl/content/locatie/biggest/eifelschnitzel.png', []),
+    new Recipe('Schnitzel', 'Very tasty', 'http://www.giftforyou.nl/content/locatie/biggest/eifelschnitzel.png', [
+      new Ingredient('French Fries', 2),
+      new Ingredient('Pork Meat', 1)
+      ]),
     new Recipe('Summer Salad', 'Okay', 'http://ohmyveggies.com/wp-content/uploads/2013/06/the_perfect_summer_salad.jpg', [])
   ];
   
