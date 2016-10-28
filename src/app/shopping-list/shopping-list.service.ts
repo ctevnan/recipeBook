309a3c5f31b8e370cbfb671e64ab89357a186a16
ctevnan <carolyntevnan@yahoo.com>
2016-10-28 11:35:39 -0400

    adding import of ingredient and storing ingredients currently on the shopping list

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@angular/core';
+import { Ingredient } from "../shared/ingredient";
 
-@Injectable()
 export class ShoppingListService {
+  private items: Ingredient[]
   
   constructor() {}
 }
