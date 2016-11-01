c195329c8112856dba3ea25984f65303d9da67d7
ctevnan <carolyntevnan@yahoo.com>
2016-11-01 13:39:20 -0400

    adding recipestartcomponent to routes

---
+++ src/app/recipes/recipes.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from "@angular/router";
+
+import { RecipeStartComponent } from "./recipe-start.component";
+
+export const RECIPE_ROUTES: Routes = [
+  { path: '', component: RecipeStartComponent }
+];
