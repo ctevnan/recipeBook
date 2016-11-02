711ccd8fbd072d029924a69e5dc108cc341ce839
ctevnan <carolyntevnan@yahoo.com>
2016-11-02 11:35:49 -0400

    importing subscription, adding private recipeIndex

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
+import { Subscription } from "rxjs/Rx";
 
 import { Recipe } from '../recipe';
 import { ShoppingListService } from "../../shopping-list";
@@ -10,11 +11,18 @@ import { ShoppingListService } from "../../shopping-list";
   templateUrl: 'recipe-detail.component.html',
 })
 export class RecipeDetailComponent implements OnInit {
+  private subscription: Subscription;
+  private recipeIndex: number;
   selectedRecipe: Recipe;
   
   constructor(private sls: ShoppingListService, private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.subscription = this.route.params.subscribe(
+      (params: any) => {
+        this.recipeIndex = params['id'];
+      }
+    );
   }
 
   onAddToShoppingList(){
